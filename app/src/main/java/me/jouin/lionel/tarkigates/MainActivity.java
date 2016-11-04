faac42af6a6c089c6c5da2e66d67d8dd08b4b0d3
Lionel Jouin <lionel.jouin@outlook.com>
2016-11-04 23:45:25 +0100

    ajout boutons

--- app/src/main/java/me/jouin/lionel/tarkigates/MainActivity.java
+++ app/src/main/java/me/jouin/lionel/tarkigates/MainActivity.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
-import android.widget.Button;
 import android.widget.Toast;
 
 import me.jouin.lionel.tarkigates.levels.Level;
@@ -16,14 +15,20 @@ public class MainActivity extends AppCompatActivity {
     public static final Positions positions = new Positions();
 
     public GameView gameView;
+    public Level level_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Level level_1 = new Level_1();
+        level_1 = new Level_1();
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
         if (level_1.isValid()) {
 
             gameView = new GameView(this, level_1);
@@ -33,11 +38,9 @@ public class MainActivity extends AppCompatActivity {
             int height = level_1.getNbSwitchs() * positions.switchSize + (level_1.getNbSwitchs() + 2) * positions.spaceBetweenComponents;
             wv.loadData("<html style='height:" + height + "px;width:3050px;'>htryy</html>", "text/html; charset=utf-8", "UTF-8");
             wv.addView(gameView);
-            wv.addView(new Button(this));
             setContentView(wv);
         } else {
             Toast.makeText(getApplicationContext(), "Level invalide", Toast.LENGTH_LONG).show();
         }
-
     }
 }
--- app/src/main/java/me/jouin/lionel/tarkigates/ui/GameView.java
+++ app/src/main/java/me/jouin/lionel/tarkigates/ui/GameView.java
@@ -4,32 +4,47 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import me.jouin.lionel.tarkigates.MainActivity;
+import me.jouin.lionel.tarkigates.R;
 import me.jouin.lionel.tarkigates.levels.Level;
 
 /**
  * Created by lione on 31/10/2016.
  */
 
-public class GameView extends View {
+public class GameView extends RelativeLayout {
 
     private Paint paint = new Paint();
     private Level level;
+    private Context context;
+    public List<Button> buttons = new ArrayList<>();
 
     public GameView(Context context, Level level) {
         super(context);
         this.level = level;
+        this.context = context;
+
+        ImageView light = new ImageView(context);
+        light.setImageResource(R.drawable.and);
+        addView(light);
+
         for (int i = 0;i<level.getNbSwitchs();i++) {
-
+            Button b = new Button(context);
+            buttons.add(b);
+            b.setY((MainActivity.positions.spaceBetweenComponents+MainActivity.positions.switchSize)*i);
+            b.setText(""+i);
+            addView(b);
         }
-    }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int width = 3050;
-        int height = 3000 + 50;
-        setMeasuredDimension(width, height);
+        RelativeLayout.LayoutParams a = new RelativeLayout.LayoutParams(3000, 3000);
+        this.setLayoutParams(a);
     }
 
     @Override
@@ -48,4 +63,5 @@ public class GameView extends View {
         canvas.drawRect(300, 2000, 1500, 3000, paint);
 
     }
+
 }
